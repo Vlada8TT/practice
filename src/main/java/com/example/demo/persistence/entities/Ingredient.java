95b3a972c6c46c389b76fd088e25881eca513c27
Yarkovskiy <yarkovskiy.aleksandr@gmail.com>
2024-06-14 13:05:55 +0300

    Added entity classes

---
+++ src/main/java/com/example/demo/persistence/entities/Ingredient.java
@@ -0,0 +1,24 @@
+package com.example.demo.persistence.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Table(name = "ingredients")
+@Data
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 20,
+    nullable = false)
+    private String name;
+
+    @ManyToMany(mappedBy = "ingredients")
+    private List<Product> products;
+
+}
---
+++ src/main/java/com/example/demo/persistence/entities/Role.java
@@ -0,0 +1,18 @@
+package com.example.demo.persistence.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "roles")
+@Data
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 20)
+    private String name;
+
+}
