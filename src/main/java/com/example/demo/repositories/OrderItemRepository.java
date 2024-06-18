16e36798294c9f6ef088eed6f08012858c529277
lustikkkkk <kirill.lustoff@gmail.com>
2024-06-18 13:48:56 +0300

    исправил все недочеты

--- src/main/java/com/example/demo/repositories/OrderItemRepository.java
+++ src/main/java/com/example/demo/repositories/OrderItemRepository.java
@@ -1,8 +1,12 @@
 package com.example.demo.repositories;
 
 
+import com.example.demo.persistence.entity.OrderItem;
+import com.example.demo.persistence.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface OrderItemRepository extends JpaRepository<OrderItem, Integer> {
--- src/main/java/com/example/demo/repositories/ProductRepository.java
+++ src/main/java/com/example/demo/repositories/ProductRepository.java
@@ -1,9 +1,12 @@
 package com.example.demo.repositories;
 
 
+import com.example.demo.persistence.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 
 
 @Repository
