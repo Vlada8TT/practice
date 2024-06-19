7ab384d32772ecaec8bb350f0dc48d649eae0730
CubicShark <fathershark004@gmail.com>
2024-06-19 21:21:40 +0300

    added thrown exceptions in UserService and renamed exceptions

--- src/main/java/com/example/demo/repositories/UserRepository.java
+++ src/main/java/com/example/demo/repositories/UserRepository.java
@@ -14,4 +14,5 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
     List<User> findAllByRole(Role roleName);
     Optional<User> findByName(String name);
+    Optional<User> findByMobilePhone(String name);
 }
