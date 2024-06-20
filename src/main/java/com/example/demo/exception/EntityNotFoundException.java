526e7cafba9d09b249ab283805be6fd9577b8a5a
lustikkkkk <kirill.lustoff@gmail.com>
2024-06-20 19:35:33 +0300

    delete method EntityNotFoundException by email

--- src/main/java/com/example/demo/exception/EntityNotFoundException.java
+++ src/main/java/com/example/demo/exception/EntityNotFoundException.java
@@ -3,11 +3,7 @@ package com.example.demo.exception;
 public class EntityNotFoundException extends RuntimeException{
 
     public EntityNotFoundException(String entityName, Integer id){
-        super(String.format(ErrorMessages.ENTITY_NOT_FOUND_BY_ID_MESSAGE, entityName, id));
-    }
-
-    public EntityNotFoundException(String entityName, String email){
-        super(String.format(ErrorMessages.ENTITY_NOT_FOUND_BY_INFO_MESSAGE, entityName, email));
+        super(String.format(ErrorMessages.ENTITY_NOT_FOUND_MESSAGE, entityName, id));
     }
 
 }
--- src/main/java/com/example/demo/exception/ErrorMessages.java
+++ src/main/java/com/example/demo/exception/ErrorMessages.java
@@ -5,8 +5,7 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class ErrorMessages {
-    public static final String ENTITY_NOT_FOUND_BY_INFO_MESSAGE = "The %s with %s not found";
-    public static final String ENTITY_NOT_FOUND_BY_ID_MESSAGE = "The %s with id %d not found";
+    public static final String ENTITY_NOT_FOUND_MESSAGE = "The %s with id %d not found";
     public static final String RESOURCE_ALREADY_EXIST_MESSAGE = "The %s with %s already exist";
     public static final String INTERNAL_ERROR = "Internal error";
     public static final String VALIDATION_FAILED = "Validation failed";
