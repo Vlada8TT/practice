5be3d40c0c8fa336e96aa2a0d706165e27a6572e
Zhizz <workotov74@gmail.com>
2024-06-21 00:22:46 +0300

    Changed productController to conform to other controllers

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -2,10 +2,10 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.request.ProductRequestDto;
 import com.example.demo.dto.response.ProductResponseDto;
+import com.example.demo.dto.validation.OnCreate;
 import com.example.demo.service.ProductService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,23 +17,25 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping
-    public ResponseEntity<ProductResponseDto> createPizza(@Valid @RequestBody ProductRequestDto productDto) {
-        return ResponseEntity.ok(productService.createProduct(productDto));
+    public ProductResponseDto createProduct(@Validated(OnCreate.class) @RequestBody ProductRequestDto productDto) {
+        ProductResponseDto createdProduct = productService.createProduct(productDto);
+        return createdProduct;
     }
 
     @GetMapping
-    public ResponseEntity<List<ProductResponseDto>> getAllPizzas() {
-        return ResponseEntity.ok(productService.getAllProducts());
+    public List<ProductResponseDto> getAllProducts() {
+        List<ProductResponseDto> products = productService.getAllProducts();
+        return products;
     }
 
-        @GetMapping("/{id}")
-        public ResponseEntity<ProductResponseDto> getPizzaById(@PathVariable int id) {
-            return ResponseEntity.ok(productService.getProductById(id));
-        }
+    @GetMapping("/{id}")
+    public ProductResponseDto getProductById(@PathVariable int id){
+        ProductResponseDto product = productService.getProductById(id);
+        return product;
+    }
 
     @DeleteMapping("/{id}")
     public void deleteById(@PathVariable int id){
         productService.deleteProduct(id);
     }
-
 }
