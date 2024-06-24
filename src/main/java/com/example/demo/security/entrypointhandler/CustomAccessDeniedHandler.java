a9a8a3c93141603cd2eb3893f18c96618933f7a3
Yarkovskiy <yarkovskiy.aleksandr@gmail.com>
2024-06-25 00:24:07 +0300

    Added classes for handling exceptions during security filtering

---
+++ src/main/java/com/example/demo/security/entrypointhandler/CustomAccessDeniedHandler.java
@@ -0,0 +1,36 @@
+package com.example.demo.security.entrypointhandler;
+
+import com.example.demo.dto.exception.ExceptionBody;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void handle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AccessDeniedException accessDeniedException)
+            throws IOException {
+
+        ExceptionBody exceptionBody = ExceptionBody.builder()
+                .status(HttpStatus.FORBIDDEN.value())
+                .message("Access denied")
+                .build();
+
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getWriter().write(objectMapper.writeValueAsString(exceptionBody));
+    }
+}
---
+++ src/main/java/com/example/demo/security/entrypointhandler/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,36 @@
+package com.example.demo.security.entrypointhandler;
+
+import com.example.demo.dto.exception.ExceptionBody;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void commence(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException authException)
+            throws IOException {
+
+        ExceptionBody exceptionBody = ExceptionBody.builder()
+                .status(HttpStatus.UNAUTHORIZED.value())
+                .message("You are not unauthorized")
+                .build();
+
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getWriter().write(objectMapper.writeValueAsString(exceptionBody));
+    }
+}
