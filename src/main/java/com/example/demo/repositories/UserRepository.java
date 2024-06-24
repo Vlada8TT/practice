88eaedd83ad51c5602278b94c82e8689f3fb8df3
Yarkovskiy <yarkovskiy.aleksandr@gmail.com>
2024-06-25 00:19:30 +0300

    Removed unused imports

--- src/main/java/com/example/demo/repositories/UserRepository.java
+++ src/main/java/com/example/demo/repositories/UserRepository.java
@@ -1,6 +1,5 @@
 package com.example.demo.repositories;
 
-
 import com.example.demo.persistence.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
