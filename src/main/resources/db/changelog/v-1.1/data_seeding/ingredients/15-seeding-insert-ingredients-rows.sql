5491ac5c4e9f7a4bdc730b9e995ef954be7a9b64
Yarkovskiy <yarkovskiy.aleksandr@gmail.com>
2024-06-28 15:35:42 +0300

    Added migrations for ingredients

---
+++ src/main/resources/db/changelog/v-1.1/data_seeding/ingredients/15-seeding-insert-ingredients-rows.sql
@@ -0,0 +1,23 @@
+INSERT INTO ingredients (name)
+VALUES ('tomato sauce'),
+       ('garlic'),
+       ('basil'),
+       ('oregano'),
+       ('mozzarella cheese'),
+       ('parmesan cheese'),
+       ('chicken'),
+       ('bacon'),
+       ('ham'),
+       ('pepperoni'),
+       ('sausage'),
+       ('anchovies'),
+       ('ground beef'),
+       ('mushrooms'),
+       ('onions'),
+       ('green peppers'),
+       ('black olives'),
+       ('spinach'),
+       ('jalapenos'),
+       ('pineapple')
+
+GO
