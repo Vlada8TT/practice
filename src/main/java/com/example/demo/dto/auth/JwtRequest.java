4c2e2a79c2a8037bccb3919f828285dbe937145b
Andrew <kylbitskiy2004@mail.ru>
2024-07-01 15:46:57 +0300

    Added @schema annotation for jwt DTOs

--- src/main/java/com/example/demo/dto/auth/JwtRequest.java
+++ src/main/java/com/example/demo/dto/auth/JwtRequest.java
@@ -3,12 +3,13 @@ package com.example.demo.dto.auth;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
+@Schema(description = "Jwt request DTO")
 public record JwtRequest(
         @NotNull(message = "Email must be not null.")
-        @Schema(description = "email", example = "johndoe@gmail.com")
+        @Schema(description = "User email", example = "user@gmail.com")
         String username,
 
-        @Schema(description = "password", example = "12345")
+        @Schema(description = "User password", example = "12345")
         @NotNull(message = "Password must be not null.")
         String password
 ) {
