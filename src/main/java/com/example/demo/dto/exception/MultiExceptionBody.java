926e1ace16d79f784fe9b7cdd0ff5a3c526d3c29
Andrew <kylbitskiy2004@mail.ru>
2024-07-01 15:46:44 +0300

    Added @schema annotation for exception DTOs

--- src/main/java/com/example/demo/dto/exception/MultiExceptionBody.java
+++ src/main/java/com/example/demo/dto/exception/MultiExceptionBody.java
@@ -1,16 +1,21 @@
 package com.example.demo.dto.exception;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 import java.util.Map;
 
+@Schema(description = "Multi exception body DTO")
 @Builder
 public record MultiExceptionBody(
 
+        @Schema(description = "Status id", example = "400")
         Integer status,
 
+        @Schema(description = "Exception message", example = "Bad Request")
         String message,
 
+        @Schema(description = "Errors")
         Map<String, String> errors
 ) {
 }
