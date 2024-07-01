dd6e47542b162efa285f3b13bd35707c6c53ff51
Andrew <kylbitskiy2004@mail.ru>
2024-07-01 15:46:31 +0300

    Added @schema annotation for request DTOs

--- src/main/java/com/example/demo/dto/request/OrderItemRequestDto.java
+++ src/main/java/com/example/demo/dto/request/OrderItemRequestDto.java
@@ -2,12 +2,15 @@ package com.example.demo.dto.request;
 
 import com.example.demo.dto.validation.OnCreate;
 import com.example.demo.dto.validation.OnUpdate;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 public record OrderItemRequestDto(
+        @Schema(description = "Product id", example = "1")
         @NotNull(message = "{product_id.notnull}", groups = {OnCreate.class, OnUpdate.class})
         Integer productId,
 
+        @Schema(description = "Product quantity", example = "2")
         @NotNull(message = "{quantity.notnull}", groups = {OnCreate.class, OnUpdate.class})
         Integer quantity
 ) {
