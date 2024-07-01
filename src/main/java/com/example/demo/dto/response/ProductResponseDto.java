dc3c32bd75418b4a8685cf64832e93d2b3d48bf8
Andrew <kylbitskiy2004@mail.ru>
2024-07-01 15:45:47 +0300

    Added @schema annotation for response DTOs

--- src/main/java/com/example/demo/dto/response/ProductResponseDto.java
+++ src/main/java/com/example/demo/dto/response/ProductResponseDto.java
@@ -1,18 +1,25 @@
 package com.example.demo.dto.response;
 
 import com.example.demo.persistence.entity.Category;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.math.BigDecimal;
 
+@Schema(description = "Product response DTO")
 public record ProductResponseDto(
+        @Schema(description = "Product id", example = "1")
         Integer id,
 
+        @Schema(description = "Product name", example = "Cola")
         String name,
 
+        @Schema(description = "Product price", example = "3.99")
         BigDecimal price,
 
+        @Schema(description = "Product category")
         Category category,
 
+        @Schema(description = "Product image path")
         String imagePath
 ) {
 }
