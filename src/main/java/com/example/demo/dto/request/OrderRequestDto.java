612aaba6749cc1c96e401465d7ebf3d0b8eabdec
Yarkovskiy <yarkovskiy.aleksandr@gmail.com>
2024-07-03 14:47:47 +0300

    LocalDateTime -> Timestamp

--- src/main/java/com/example/demo/dto/request/OrderRequestDto.java
+++ src/main/java/com/example/demo/dto/request/OrderRequestDto.java
@@ -5,16 +5,16 @@ import com.example.demo.dto.validation.OnUpdate;
 import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
-import java.time.LocalDateTime;
+
+import java.sql.Timestamp;
 
 public record OrderRequestDto(
         @NotNull(message = "{user_id.notnull}", groups = {OnCreate.class, OnUpdate.class})
-        @Length(max = 20, message = "{user_id.length}", groups = {OnCreate.class, OnUpdate.class})
         Integer userId,
 
         @NotNull(message = "{time.notnull}", groups = {OnCreate.class, OnUpdate.class})
-        @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
-        LocalDateTime orderDate,
+        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+        Timestamp orderDate,
 
         @NotNull(message = "{status.notnull}", groups = {OnCreate.class, OnUpdate.class})
         @Length(max = 20, message = "{status.length}", groups = {OnCreate.class, OnUpdate.class})
